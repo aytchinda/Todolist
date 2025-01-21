15455ed06d3b072513edc9aadaac15067326988d
Aytchinda <aymardrinel@outlook.fr>
2025-01-21 16:29:14 +0100

    initialisation de la mise a jour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,63 @@
 window.onload = () => {
     const form = document.getElementById('todoForm');
+    const ul = document.getElementById('todoList');
     let todos = [];
 
-    const handleDelete = ({id}) => {
-        console.log("ID to delete:", id);
-        todos = todos.filter(todo  => todo.id !== id);
+   
+
+    const handleUpdate = (todo) => {
+        if (!todo.isUpdating) {
+              const index = todos.findIndex(t => t.id === todo.id);
+        todos[index].isUpdating = true;
+        }
+      
+        refetch();
+        console.log(todo);
+    };
+    const handleDelete = ({ id }) => {
+        todos = todos.filter(todo => todo.id !== id);    
         refetch();
     };
     const refetch = () => {
-        // const ul = document.querySelector('ul');
-        const ul = document.getElementById('todoList');
-       
         ul.innerHTML = '';
         todos.forEach((todo) => {
-           
             const li = document.createElement('li');
-            const span = document.createElement('span');
-            span.innerHTML = todo.name;
-            const updateBtn = document.createElement('button');
-            updateBtn.innerText = 'Update';
-            updateBtn.className = 'btn btn-primary';
+
             const deleteBtn = document.createElement('button');
-            deleteBtn.innerText = 'Delete';   
+            deleteBtn.innerText = 'Delete';
             deleteBtn.className = 'btn btn-danger';
             deleteBtn.onclick = () => handleDelete(todo);
-            
-            li.appendChild(span);
+
+            const updateBtn = document.createElement('button');
+            if (todo.isUpdating) {
+                // Editing mode
+                const input = document.createElement('input');
+                input.value = todo.name;
+                updateBtn.innerText = 'Save';
+                updateBtn.className = 'btn btn-warning';
+                updateBtn.onclick = () => {
+                    todo.name = input.value.trim();
+                    todo.isUpdating = false;
+                    refetch();
+                };
+                li.appendChild(input);
+            } else {
+                // Display mode
+                const span = document.createElement('span');
+                span.innerHTML = todo.name;
+                updateBtn.innerText = 'Update';
+                updateBtn.className = 'btn btn-primary';
+                li.appendChild(span);
+            }
+                
+            updateBtn.onclick = () => handleUpdate(todo);
             li.appendChild(updateBtn);
             li.appendChild(deleteBtn);
 
             ul.appendChild(li);
-            
         });
-    }
-
+    };
+    
     form.onsubmit = (e) => {
         e.preventDefault();
         const input = form.querySelector('input');
@@ -43,12 +67,12 @@ window.onload = () => {
                 id: Math.round(Math.random() * 9999999999),
                 name: todoName,
                 updatedAt: null,
+                isUpdating: false,
                 createdAt: new Date(),
             };
-            //on sauvegarde le todo 
             todos.push(todo);
             refetch();
         }
         form.reset();
-    }
-}
+    };
+};
