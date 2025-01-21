ce2d6f5f7fbc5ba409e034c85cd7ffca9f9a4d7e
Aytchinda <aymardrinel@outlook.fr>
2025-01-21 02:40:55 +0100

    sauvegarde de la todo et mise à jour de l'interface

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,39 @@
 window.onload = () => {
-    
+    const form = document.getElementById('todoForm');
+    let todos = [];
+    const refetch = () => {
+        // const ul = document.querySelector('ul');
+        const ul = document.getElementById('todoList');
+       
+        ul.innerHTML = '';
+        todos.forEach((todo) => {
+            ul.innerHTML += 
+            ` <li>
+                <span> ${todo.name} 5 </span>
+                <button class="btn btn-primary">Update</button>
+                <button class="btn btn-danger">Delete</button>
+            </li>
+            `
+        });
+    }
+    // form.addEventListener('submit', (e) => {});
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        const input = form.querySelector('input');
+        const todoName = input.value.trim();
+        if (todoName) {
+            const todo = {
+                id: Math.round(Math.random() * 9999999999),
+                name: todoName,
+                updatedAt: null,
+                createdAt: new Date(),
+            };
+            //on sauvegarde le todo 
+            todos.push(todo);
+            refetch();
+        }
+
+
+        form.reset();
+    }
 }
